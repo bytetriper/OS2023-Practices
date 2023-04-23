0c3fb7508dee23c7437df9399f0ab98e3e92c215
Bytetriper Server <1220167562@qq.com>
2023-04-23 14:28:26 +0800

    2-1 buddy done;

--- practice-2/practice_2-1/utils.h
+++ practice-2/practice_2-1/utils.h
@@ -1,3 +1,5 @@
+#ifndef UTILS_H
+#define UTILS_H
 #define ok(expr)                                                         \
     do {                                                                 \
         if (!((expr) || fake_mode)) {                                    \
@@ -33,3 +35,28 @@
     do {                          \
         printf("\nTest Ends.\n"); \
     } while (0)
+#include <stdio.h>
+#include <unistd.h>
+#include <malloc.h>
+#include<stdlib.h>
+void fail(const char* message, const char* function, int line);
+struct Node
+{
+    struct Node *pre, *nxt;
+    void *item;
+};
+struct LinkedList // both-direction LinkedList
+{
+    struct Node *head, *tail;
+    int size;
+};
+struct LinkedList * New_LinkedList();
+void LinkedList_Init(struct LinkedList *list);
+struct Node * LinkedList_push(struct LinkedList *list, struct Node *node);
+struct Node * LinkedList_push_item(struct LinkedList *list, void *item);
+void *LinkedList_head_item(struct LinkedList *list);
+int LinkedList_empty(struct LinkedList *list);
+struct Node *LinkedList_head(struct LinkedList *list);
+void *LinkedList_popFirst(struct LinkedList *list);
+void LinkedList_pop(struct LinkedList *list, struct Node *node);
+#endif
